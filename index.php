d75f252e73d369a82b0aaa21007d971446a6d089
19yisus <46941371+19yisus@users.noreply.github.com>
2020-05-26 00:28:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php 
-
-	echo "el cono";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Primer test Heroku</title>
+</head>
+<body>
+	<h1>Yisus</h1>
+</body>
+</html>
