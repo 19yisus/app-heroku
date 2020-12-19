d2ba013f176481a544e88e06f35e2220a2b3a4f9
Jesus morales <46941371+19yisus@users.noreply.github.com>
2020-12-18 23:38:47 -0400

    hola

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Primer test Heroku</title>
-</head>
-<body>
-	<h1>Primer test de Heroku</h1>
-	<h3>Hello world</h3>
-</body>
-</html>
+<?php
+	echo 'hola';
